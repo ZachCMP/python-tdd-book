9609dc28f9bb146658bda12b918e03acab0ed39f
ZachCMP <zparks88@gmail.com>
2019-07-31 19:22:04 -0400

    Refactored functional tests, adding a helper method to remove duplication

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Jon has heard about a cool new online to-do app.  She goes to check out its homepage.
         self.browser.get('http://127.0.0.1:8000')
@@ -34,10 +39,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Eat an entire chair" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Eat an entire chair', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Eat an entire chair')
 
         # There is still a text box inviting him to add another item.  He enters "Collapse into nothing"
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -46,10 +48,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Eat an entire chair', [row.text for row in rows])
-        self.assertIn('2: Collapse into nothing', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Eat an entire chair')
+        self.check_for_row_in_list_table('1: Collapse into nothing')
 
         # Jon wonders whether the site will remember his list.  Then he sees that the site has 
         # generated a unique URL for him -- there is some explanatory text to that effect.
