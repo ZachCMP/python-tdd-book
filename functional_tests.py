ddaac7c2a86a94a178878f822d62a911dd7d7c5e
ZachCMP <zparks88@gmail.com>
2019-07-31 14:21:18 -0400

    Functional test not checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,16 +17,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices that the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # He is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # He types "Eat an entire chair" into a text box
+        inputbox.send_keys('Eat an entire chair')
 
         # When he hits enter, the page updates, and now the page lists:
         # "1: Eat an entire chair" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Eat an entire chair' for row in rows)
+        )
 
         # There is still a text box inviting him to add another item.  He enters "Collapse into nothing"
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
